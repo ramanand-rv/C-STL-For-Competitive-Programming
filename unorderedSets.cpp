eecd7d8421fc12c4bf4c46dfeba8e74ba98f05dd
Ramanand Thakur <80584888+ramanand-rv@users.noreply.github.com>
2022-01-02 07:17:59 +0530

    Add files via upload

---
+++ unorderedSets.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+#define F first
+#define S second
+#define PI 3.141592653589793238462
+#define countBits(x) __builtin_popcount(x)
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define mt make_tuple
+#define eb emplace_back
+#define np next_permutation
+#define prio priority_queue
+#define ll long long int
+#define vi vector<ll, ll> 
+#define pii pair<ll, ll>
+typedef unsigned long long ull;
+typedef long double lld;
+#define B begin()
+#define E end()
+#define um unordered_map
+#define us unordered_set
+
+
+void print(us<string>&s){
+	for(auto value: s){
+		cout<<value<<endl;
+	}
+}
+
+
+int main() {
+us<string> s;		// O(1)
+s.insert("abc");
+s.insert("pqr");
+s.insert("xyz");
+s.insert("zxc");
+
+print(s);
+
+
+
+
+
+
+}
